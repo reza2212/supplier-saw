0990a5df42491eb4ee373c18a72f79abd01b952e
brianv4 <55471120+brianv4@users.noreply.github.com>
2021-07-18 14:23:11 +0700

    fix2

--- dist/admin/normalisasi.php
+++ dist/admin/normalisasi.php
@@ -17,7 +17,7 @@ $stmt = $pro->readKhusus();
           <div id="layoutSidenav_content">
                 <main>
                     <div class="container-fluid">
-                        <h1 class="mt-4">Dashboard</h1>
+                        <h1 class="mt-4">Perankingan</h1>
                         <ol class="breadcrumb mb-4">
                             <li class="breadcrumb-item active">Normalisasi R Perangkingan</li>
                         </ol>
